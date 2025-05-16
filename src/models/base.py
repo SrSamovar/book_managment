e938cc8d7a37f82dce3607d80cf092dd53cc97cd
SrSamovar <levchenkovadim2001@mail.ru>
2025-05-17 00:21:56 +0300

    add init and close orm

--- src/models/base.py
+++ src/models/base.py
@@ -25,3 +25,12 @@ class Base(DeclarativeBase, AsyncAttrs):
     updated_at: Mapped[datetime] = mapped_column(DateTime,
                                                  server_default=func.now(),
                                                  onupdate=func.now())
+
+
+async def init_orm():
+    with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+
+
+async def close_orm():
+    await engine.dispose()
